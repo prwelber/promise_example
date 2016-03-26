4d8f3d032cb8982cb3e7a19ff989a2c66b42c0ec
Philip Welber <prwelber@gmail.com>
2016-03-26 14:32:07 -0400

    another commit

--- main.js
+++ main.js
@@ -41,9 +41,27 @@ getUserWithBluebird(1)
 // callback for then receives whatever was passed into the
 // resolve function of the promise
 .then(function(usernameData) {
-	console.log('got username with bluebird:', usernameData)
+	console.log('got username with bluebird:', usernameData);
+	return usernameData + ' is the name of a user!!!! SICK!';
+})
+.then(function(newUsernameData) {
+	console.log('check out the new data', newUsernameData);
 })
 .catch(function (error) {
 	console.log('error getting user with bluebird', error);
-});
+})
+.finally(function () {
+	console.log('this gets run no matter what, error or not');
+})
+
+/*
+Essentially, the getUser function returns a promise object, inside of which
+things have happened (an api call), and then when you call that function,
+you can attach .then and .catch to it. The callback in the .then block
+will take whatever value was passed to resolve() in the original function.
+For chaining thens, whatever is returned in the .then block will be passed
+to the next .then block.
+And not only can you return single values from each .then block, you can
+also create and return new promises
+*/
 
